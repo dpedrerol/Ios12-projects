8c10d29be94d25dd96489bb541cb98d2f7194ae1
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-08-19 20:00:09 +0200

    Initial Commit

---
+++ 02-I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-I am Poor
+//
+//  Created by Carlos Daniel Pedrerol on 19/08/2020.
+//  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
