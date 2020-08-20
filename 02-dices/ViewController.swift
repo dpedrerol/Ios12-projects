aafbf8912cf05c3b675a3c2364b72ec458b81352
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-08-20 12:32:57 +0200

    Initial Commit

---
+++ 02-dices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Carlos Daniel Pedrerol on 20/08/2020.
+//  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
