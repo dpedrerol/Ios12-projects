f44fb7ce01656c23fa1c8d43c8640e687d1637f1
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-08-26 14:05:35 +0200

    3 Vinculada la vista al view Controller 4 Programada la clase

--- 03-Adivino/ViewController.swift
+++ 03-Adivino/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    
+    @IBOutlet weak var labelAnswer: UILabel!
+    
+    
+    let answersArray = ["Si", "No", "Depende", "Puede ser", "Pregunta otra vez", "Ni lo sueñes!", "JaJaJa!"]
+    
+    var randomAnswerNumber: Int = 0
+    
+    let nAnswers : UInt32
+    
+    required init?(coder aDecoder: NSCoder) {
+        nAnswers = UInt32(answersArray.count)
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        generateRandomAnswers()
+    }
+    
+    func generateRandomAnswers(){
+        self.randomAnswerNumber = Int(arc4random_uniform(nAnswers))
+        
+        let nameLabelAnswer = answersArray[randomAnswerNumber]
+    
+        self.labelAnswer.text = nameLabelAnswer
+        
+        
+    }
+
 }
+
+
+
+
 
