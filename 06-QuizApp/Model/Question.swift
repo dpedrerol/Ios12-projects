6bcfe625607a62eff41fe77a37b1003d73f5fdd4
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-09-16 11:40:50 +0200

    Agregados sonidos de éxito o fracaso al responder las preguntas según el requerimiento de la pregunta 1 de la tarea

---
+++ 06-QuizApp/Model/Question.swift
@@ -0,0 +1,43 @@
+//
+//  Question.swift
+//  06-QuizApp
+//
+//  Created by Carlos Daniel Pedrerol on 11/09/2020.
+//  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
+//
+
+import Foundation
+
+class Question : CustomStringConvertible, Codable {
+    
+    let question : String
+    let answer : Bool
+    let explanation :String
+    
+    /*enum CodingKeys : String, CodingKey {
+        case questionText = "question"
+        case answer = "answer"
+        case answerExplanation = "explanation"
+    }*/
+    
+    var description : String {
+        let respuesta = (answer ? "Verdadera" : "Falsa")
+        return """
+        Pregunta:
+        =========
+            - \(question)
+            - Respuesta: \(respuesta)
+            - Explicación: \(explanation)
+        """
+    }
+    
+    init(text: String, correctAnswer: Bool, answer : String) {
+        self.question = text
+        self.answer = correctAnswer
+        self.explanation = answer
+    }
+}
+struct QuestionsBank : Codable {
+    var questions : [Question]
+    }
+
---
+++ 06-QuizApp/Model/QuestionsFactory.swift
@@ -0,0 +1,55 @@
+//
+//  File.swift
+//  06-QuizApp
+//
+//  Created by Carlos Daniel Pedrerol on 11/09/2020.
+//  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
+//
+
+import Foundation
+
+class QuestionsFactory {
+    
+    var questionsBank : QuestionsBank!
+    
+    init(){
+        //Procesado manual de plist
+        /*if let path = Bundle.main.path(forResource: "QuestionBank", ofType: "plist"){
+            if let plist = NSDictionary(contentsOfFile: path){
+                let questionData = plist["Questions"] as! [AnyObject]
+                
+                for question in questionData {
+                    if let text = question["question"], let ans = question["answer"], let expl = question["explanation"] {
+                        questions.append(question(text: text as! String, correctAnswer: ans as! Bool, answer: expl as! String))
+                    }
+                }
+            }
+        }*/
+       
+        //Procesado automático de plist con codable
+        do {
+            if let url = Bundle.main.url(forResource: "QuestionBank", withExtension: "plist") {
+                let data = try Data(contentsOf: url)
+                self.questionsBank = try PropertyListDecoder().decode(QuestionsBank.self, from: data)
+               
+        }
+        }catch{
+                print(error)
+            }
+        }
+    
+     func getQuestionAt(index : Int) -> Question? {
+     if index<0 || index>=self.questionsBank.questions.count {
+         return nil
+     }else {
+         return self.questionsBank.questions[index]
+            
+        }
+    }
+    func getRandomQuestion() -> Question {
+        let index = Int(arc4random_uniform(UInt32(self.questionsBank.questions.count)))
+        return self.questionsBank.questions[index]
+    
+    }
+    
+}
