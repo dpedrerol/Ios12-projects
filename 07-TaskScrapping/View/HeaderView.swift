fe606fb50a7f022e734502af6bb480ca76625044
Daniel Pedrerol <65600988+dpedrerol@users.noreply.github.com>
2020-10-23 19:10:55 +0200

    Actualizadas las vistas

--- 07-TaskScrapping/View/HeaderView.swift
+++ 07-TaskScrapping/View/HeaderView.swift
@@ -2,15 +2,37 @@
 //  HeaderView.swift
 //  07-TaskScrapping
 //
-//  Created by Carlos Daniel Pedrerol on 19/10/2020.
+//  Created by Carlos Daniel Pedrerol on 22/10/2020.
 //  Copyright © 2020 Carlos Daniel Pedrerol. All rights reserved.
 //
 
 import UIKit
 
 class HeaderView: UICollectionReusableView {
-        
+    
     
     @IBOutlet weak var labelHeader: UILabel!
     
+}
+
+class FooterView: UICollectionReusableView {
+    
+    @IBOutlet weak var buttomPage: UISegmentedControl!
+    
+    
+    
+   
+    @IBAction func changePage(_ sender: Any) {
+    
+        /*
+             if  buttomPage.selectedSegmentIndex == 0 {
+                     
+                 }else { buttomPage.selectedSegmentIndex == 1 {
+                 
+                 }*/
+    
+    }
+    
+    
+        
 }
